c5b7faed7d1c01de85dff3d42663d3a2abbbbdde
Visoar Zhang <visoarcn@gmail.com>
2023-06-30 11:54:43 +0800

    Open homepage link

--- website/src/components/homepage/HomeSection.tsx
+++ website/src/components/homepage/HomeSection.tsx
@@ -80,28 +80,29 @@ export default function HeroSection() {
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <div className="mx-auto max-w-4xl text-center">
               <h1 className="text-6xl font-bold bg-gradient-to-r from-[#F303C8] via-[#03B6F1] to-[#03F7E7] text-transparent bg-clip-text leading-tight">
-                BifroMQ 
                 <span className="text-gray-300"> 🚀 </span>
-                <Translate>Comming soon</Translate>
+                BifroMQ 
+                {/* <Translate>OpenSource</Translate> */}
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
                 <b>BifroMQ </b><Translate>is a high-performance MQTT Broker messaging middleware that utilizes Serverless architecture. Open-sourced by Baidu.</Translate>
               </p>
-              <div className="mt-6">
+              {/* <div className="mt-6">
                 <a href="https://iwenjuan.baidu.com/?code=kw9gi9" target="_blank" 
                    className="text-sm font-semibold leading-6 text-blue-600 dark:text-blue-300">
                 <Translate>Join the waitlist</Translate>
                   <span aria-hidden="true">→</span>
                 </a>
-              </div>
-              {/* <div className="mt-10 flex items-center justify-center gap-x-6"> */}
-                {/* <a href="#" className="text-white rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold  shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-                  Get started
+              </div> */}
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a href="https://github.com/baidu/bifromq" target='_blank' className="text-white inline-flex items-center rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold  shadow-sm hover:bg-blue-600 hover:text-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+                  <svg fill="currentColor" viewBox="0 0 24 24" className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd"></path></svg>
+                  Github 
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6">
-                  Learn more <span aria-hidden="true">→</span>
-                </a> */}
-              {/* </div> */}
+                <a href="./docs/Readme/" className="text-sm font-semibold leading-6">
+                  <Translate>Get Started</Translate> <span aria-hidden="true">→</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
