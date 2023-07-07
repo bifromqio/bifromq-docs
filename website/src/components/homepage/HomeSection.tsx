20b41d4311904daf42b7fd7b69c575aab08b5748
Visoar Zhang <visoarcn@gmail.com>
2023-07-07 14:22:34 +0800

    fix index Horizontal scrollbar

--- website/src/components/homepage/HomeSection.tsx
+++ website/src/components/homepage/HomeSection.tsx
@@ -65,11 +65,11 @@ export default function HeroSection() {
           <rect width="100%" height="100%" strokeWidth={0} fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" />
         </svg>
         <div
-          className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+          className="absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
           aria-hidden="true"
         >
           <div
-            className="aspect-[1108/632] md:w-[69.25rem] w-screen bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+            className="aspect-[1108/532] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
             style={{
               clipPath:
                 'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
