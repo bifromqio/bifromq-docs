2efab1ea4d0a7c4adfd0bac5fa31b7a203e6c71e
Visoar Zhang <visoarcn@gmail.com>
2023-07-17 13:22:34 +0800

    add:mailto address & icon

--- website/src/components/homepage/HomeFooter.tsx
+++ website/src/components/homepage/HomeFooter.tsx
@@ -4,6 +4,17 @@ import { useColorMode } from '@docusaurus/theme-common';
 const footerNavigation = {
   social: [
     {
+      name: 'Mail',
+      href: 'mailto:hello+web@bifromq.io',
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 1024 1024" {...props}>
+            <path 
+            d="M512 576a32 32 0 0 1-17.184-4.992L128 337.568v430.656A64 64 0 0 0 192.064 832h639.872A64 64 0 0 0 896 768.224V337.888l-366.816 233.12A32.096 32.096 0 0 1 512 576" ></path>
+            <path d="M831.936 192H192.064A64 64 0 0 0 128 255.808v4.896l384 245.376 384-244.032v-6.24A64 64 0 0 0 831.936 192"></path>
+        </svg>
+      ),
+    },
+    {
       name: 'Discord',
       href: 'https://discord.gg/Pfs3QRadRB',
       icon: (props) => (
